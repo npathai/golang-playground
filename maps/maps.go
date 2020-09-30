d18540b506649b4b6b74ff4784821b0f7ff64b87
Narendra Pathai <narendra.pathai@gmail.com>
2020-09-30 01:23:04 -0400

    Basics of map data type in Go

---
+++ maps/maps.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	m := map[string]int{"foo": 42}
+	fmt.Println(m)
+
+	fmt.Println(m["foo"])
+
+	m["foo"] = 23
+	fmt.Println(m)
+
+	delete(m, "foo")
+	fmt.Println(m)
+}
+
