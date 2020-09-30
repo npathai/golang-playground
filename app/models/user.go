fc0f5c9c77c7770dd93334a539af48e1bf439d0b
Narendra Pathai <narendra.pathai@gmail.com>
2020-09-30 06:23:15 -0400

    Added get and add users into user model

---
+++ app/models/user.go
@@ -0,0 +1,23 @@
+package models
+
+type User struct {
+	ID int
+	FirstName string
+	LastName string
+}
+
+var(
+	users []*User
+	nextId = 1
+)
+
+func GetUsers() []*User {
+	return users
+}
+
+func AddUser(user User) (User, error) {
+	user.ID = nextId
+	nextId++
+	users = append(users, &user)
+	return user, nil
+}
