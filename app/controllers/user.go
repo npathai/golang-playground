677bbaa1b55d3c8198281a745743194d0a0a1a2a
Narendra Pathai <narendra.pathai@gmail.com>
2020-10-01 10:56:07 -0400

    Completed creating user web service with Go lang

--- app/controllers/user.go
+++ app/controllers/user.go
@@ -1,8 +1,11 @@
 package controllers
 
 import (
+	"../models"
+	"encoding/json"
 	"net/http"
 	"regexp"
+	"strconv"
 )
 
 type userController struct {
@@ -11,8 +14,108 @@ type userController struct {
 
 // This binds the method ServeHTTP with userController class, this is how we create functions on an object
 func (uc userController) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// Conversion into a Slice data type
-	w.Write([]byte("Hello from the User Controller"))
+	if r.URL.Path == "/users" {
+		switch r.Method {
+		case http.MethodGet:
+			uc.getAll(w, r)
+		case http.MethodPost:
+			uc.post(w, r)
+		default:
+			w.WriteHeader(http.StatusNotImplemented)
+		}
+	} else {
+		matches := uc.userIDPattern.FindStringSubmatch(r.URL.Path)
+		if len(matches) == 0 {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		id, err := strconv.Atoi(matches[1])
+		if err != nil {
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
+		switch r.Method {
+		case http.MethodGet:
+			uc.get(id, w)
+		case http.MethodPut:
+			uc.put(id, w, r)
+		case http.MethodDelete:
+			uc.delete(id, w)
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}
+}
+
+func (uc *userController) getAll(w http.ResponseWriter, r *http.Request) {
+	encodeResponseAsJson(models.GetUsers(), w)
+}
+
+func (uc *userController) get(id int, w http.ResponseWriter) {
+	user, err := models.GetUserById(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+	encodeResponseAsJson(user, w)
+}
+
+func (uc *userController) post(w http.ResponseWriter, r *http.Request) {
+	requestUser, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse User object"))
+		return
+	}
+	user, err := models.AddUser(requestUser)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJson(user, w)
+}
+
+func (uc *userController) put(id int, w http.ResponseWriter, r *http.Request) {
+	requestUser, err := uc.parseRequest(r)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("Could not parse User object"))
+		return
+	}
+	if id != requestUser.ID {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte("ID of submitted user must match ID in URL"))
+		return
+	}
+
+	user, err := models.UpdateUser(requestUser)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	encodeResponseAsJson(user, w)
+}
+
+func (uc *userController) delete(id int, w http.ResponseWriter) {
+	err := models.DeleteUser(id)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func (uc *userController) parseRequest(r *http.Request) (models.User, error) {
+	dec := json.NewDecoder(r.Body)
+	var user models.User
+	err := dec.Decode(&user)
+	if err != nil {
+		return models.User{}, err
+	}
+	return user, nil
 }
 
 // This is constructor function. There is no special meaning of this function, it's based on convention in Go.
