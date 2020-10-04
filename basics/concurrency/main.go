c0a58feb12b29460433e18f8548b99355d7b73e8
Narendra Pathai <narendra.pathai@gmail.com>
2020-10-04 03:42:50 -0400

    Removed data race between routines by using Mutex

--- basics/concurrency/main.go
+++ basics/concurrency/main.go
@@ -13,45 +13,51 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 
 func main() {
 	wg := &sync.WaitGroup{}
+	m := &sync.Mutex{}
+
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(6) + 1
 		// Letting the Wait group know that we are creating 2 go routines
 		wg.Add(2)
 		// adding go keyword in front of the function makes it go routine, or like a Runnable.
 		// Anonymous function and immediate call
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := queryCache(id); ok {
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+			if b, ok := queryCache(id, m); ok {
 				fmt.Println("From cache")
 				fmt.Println(b)
 			}
 			// Task letting the wait group know that it's done
 			wg.Done()
-		}(id, wg)
+		}(id, wg, m)
 
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := queryDatabase(id); ok {
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+			if b, ok := queryDatabase(id, m); ok {
 				fmt.Println("From database")
 				fmt.Println(b)
 			}
 			wg.Done()
-		}(id, wg)
+		}(id, wg, m)
 	}
 
 	// Waiting for all the routines to finish before exiting the main routine
 	wg.Wait()
 }
 
-func queryCache(id int) (book.Book, bool) {
+func queryCache(id int, m *sync.Mutex) (book.Book, bool) {
+	m.Lock()
 	book, ok := cache[id]
+	m.Unlock()
 	return book, ok
 }
 
-func queryDatabase(id int) (book.Book, bool) {
+func queryDatabase(id int, m *sync.Mutex) (book.Book, bool) {
 	// Simulate lag
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range book.Books {
 		if b.ID == id {
+			m.Lock()
 			cache[id] = b
+			m.Unlock()
 			return b, true
 		}
 	}
