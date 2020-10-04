aaca6d07d5c0a2ff83cccece244da53349e4bd99
Narendra Pathai <narendra.pathai@gmail.com>
2020-10-04 08:08:25 -0400

    Used channels and select block in cache example to make it work

--- basics/concurrency/main.go
+++ basics/concurrency/main.go
@@ -14,29 +14,49 @@ var rnd = rand.New(rand.NewSource(time.Now().UnixNano()))
 func main() {
 	wg := &sync.WaitGroup{}
 	m := &sync.RWMutex{}
+	dbChan := make(chan book.Book)
+	cacheChan := make(chan book.Book)
 
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(6) + 1
+		fmt.Printf("Id: %v\n", id)
 		// Letting the Wait group know that we are creating 2 go routines
 		wg.Add(2)
 		// adding go keyword in front of the function makes it go routine, or like a Runnable.
 		// Anonymous function and immediate call
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan <- book.Book) {
 			if b, ok := queryCache(id, m); ok {
-				fmt.Println("From cache")
-				fmt.Println(b)
+				ch <- b
 			}
 			// Task letting the wait group know that it's done
 			wg.Done()
-		}(id, wg, m)
+		}(id, wg, m, cacheChan)
 
-		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex) {
+		go func(id int, wg *sync.WaitGroup, m *sync.RWMutex, ch chan <- book.Book) {
 			if b, ok := queryDatabase(id, m); ok {
+				m.Lock()
+				cache[id] = b
+				m.Unlock()
+				ch <- b
+			}
+			wg.Done()
+		}(id, wg, m, dbChan)
+
+		// Create one go routine per query to receive the results
+		go func(cacheChan, dbChan <- chan book.Book) {
+			select {
+			case b:= <- cacheChan:
+				fmt.Println("From cache")
+				fmt.Println(b)
+				// When get the cache hit, we need to drain the db channel
+				<- dbChan
+			case b:= <- dbChan:
 				fmt.Println("From database")
 				fmt.Println(b)
 			}
-			wg.Done()
-		}(id, wg, m)
+		}(cacheChan, dbChan)
+
+		time.Sleep(150 * time.Millisecond)
 	}
 
 	// Waiting for all the routines to finish before exiting the main routine
@@ -46,9 +66,9 @@ func main() {
 func queryCache(id int, m *sync.RWMutex) (book.Book, bool) {
 	// Getting read lock to cache (shared memory)
 	m.RLock()
-	book, ok := cache[id]
+	b, ok := cache[id]
 	m.RUnlock()
-	return book, ok
+	return b, ok
 }
 
 func queryDatabase(id int, m *sync.RWMutex) (book.Book, bool) {
@@ -56,9 +76,6 @@ func queryDatabase(id int, m *sync.RWMutex) (book.Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range book.Books {
 		if b.ID == id {
-			m.Lock()
-			cache[id] = b
-			m.Unlock()
 			return b, true
 		}
 	}
