a1612028372724bdb980867de2a7d2f91f4bcecc
Narendra Pathai <narendra.pathai@gmail.com>
2020-10-05 01:41:17 -0400

    A failing test in Go

--- basics/testing/main_test.go
+++ basics/testing/main_test.go
@@ -8,7 +8,14 @@ func TestAdditionWorksInGo(t *testing.T) {
 	if got != expected {
 		t.Errorf("Did not get expected result. Got: '%v', Expected: '%v'", got, expected)
 	}
+}
 
+func TestSubtraction(t *testing.T) {
+	got := 2 - 2
+	expected := 1
+	if got != expected {
+		t.Errorf("Did not get expected result. Got: '%v', Expected: '%v'", got, expected)
+	}
 }
 
 
